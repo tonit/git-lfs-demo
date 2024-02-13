0f230d3d709fdd6b70c17972b3862424ad47a5dd
Toni Menzel <toni.menzel@rebaze.com>
2024-02-13 17:08:39 +0100

    fix demo image file naming

--- demo3.sh
+++ demo3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker build  -t git-lfs-demo2 - <<EOF
+docker build  -t git-lfs-demo3 - <<EOF
     FROM ubuntu:22.04
     RUN apt update && apt upgrade -y && apt install -y file git
     RUN git clone https://github.com/tonit/git-lfs-demo.git
 EOF
 
-docker run --rm git-lfs-demo2 /bin/bash -c "apt install -y git-lfs && cd git-lfs-demo && git lfs pull && file /git-lfs-demo/paper.pdf"
+docker run --rm git-lfs-demo3 /bin/bash -c "apt install -y git-lfs && cd git-lfs-demo && git lfs pull && file /git-lfs-demo/paper.pdf"
 
 
