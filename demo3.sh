79875e92c40ee89df5b971e56931ea0ee5842062
Toni Menzel <toni.menzel@rebaze.com>
2024-02-15 10:08:01 +0100

    simplify demo3

--- demo3.sh
+++ demo3.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-docker build  -t git-lfs-demo3 - <<EOF
-    FROM ubuntu:22.04
-    RUN apt update && apt upgrade -y && apt install -y file git
-    RUN git clone https://github.com/tonit/git-lfs-demo.git
-EOF
-
-docker run --rm git-lfs-demo3 /bin/bash -c "apt install -y git-lfs && cd git-lfs-demo && git lfs pull && file /git-lfs-demo/paper.pdf"
+docker run --rm ubuntu:22.04 /bin/bash -c " \
+    apt update && apt upgrade -y && apt install -y file git
+    git clone https://github.com/tonit/git-lfs-demo.git 
+    apt install -q -y git-lfs
+    cd git-lfs-demo
+    git lfs pull
+    file /git-lfs-demo/paper.pdf
+    "
 
 
